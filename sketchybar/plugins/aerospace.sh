4563fc15f02528687a4e23e978d7a84673e28bd9
tonytwonary <tony@twonary.com>
2025-04-22 15:35:37 -0300

    sketchy plus tmux ok

--- sketchybar/plugins/aerospace.sh
+++ sketchybar/plugins/aerospace.sh
@@ -1,17 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# make sure it's executable with:
+# chmod +x ~/.config/sketchybar/plugins/aerospace.sh
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
     sketchybar --set $NAME background.drawing=on
 else
     sketchybar --set $NAME background.drawing=off
 fi
-
-# echo "called with $1"
-# echo "$FOCUSED_WORKSPACE"
-#
-# if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-#     sketchybar --set $NAME background.drawing=on
-# else
-#     sketchybar --set $NAME background.drawing=off
-# fi
-#
